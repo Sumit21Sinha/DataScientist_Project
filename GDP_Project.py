f5499a2ef17850d6889c275ff2473d78664ccbde
Sumit <sumit525sinha@gmail.com>
2025-09-07 13:01:01 +0530

    CSV files imported and checked

--- GDP_Project.py
+++ GDP_Project.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+current_gdp=pd.read_csv("Current_GDP.csv", sep=",", on_bad_lines="skip")
+inflation=pd.read_csv("Inflation.csv", sep=",", on_bad_lines="skip")
+unemployment=pd.read_csv("Unemployment.csv", sep=",", on_bad_lines="skip")
+population=pd.read_csv("Population.csv", sep=",", on_bad_lines="skip")
+growth_gdp=pd.read_csv("GDP_growth.csv", sep=",", on_bad_lines="skip")
+print("\nThe inflation dataset/table looks like this :\n", inflation.head())
+print("\nThe inflation dataset/table looks like this :\n", unemployment.head())
+print("\nThe inflation dataset/table looks like this :\n", population.head())
+print("\nThe inflation dataset/table looks like this :\n", current_gdp.head())
+print("\nThe inflation dataset/table looks like this :\n", growth_gdp.head())
